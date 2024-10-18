cb58cd068a82c11b882e1f11c5b01573b49f8b43
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 18:52:10 +0200

    Assignment reference

--- code/index.php
+++ code/index.php
@@ -4,8 +4,9 @@ $very_bad_unclear_name = "15 chicken wings";
 
 
 // Write your code here:
+$order = &$very_bad_unclear_name; // new variable as a reference
 
-
+$order .= " and 1 can of Coke"; // extension string
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
