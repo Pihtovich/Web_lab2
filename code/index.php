c6ed6be741db87615b534896cd94b07973188d51
Pihtovich <86502038+Pihtovich@users.noreply.github.com>
2024-10-18 18:56:46 +0200

    Numbers

--- code/index.php
+++ code/index.php
@@ -6,7 +6,29 @@ $very_bad_unclear_name = "15 chicken wings";
 // Write your code here:
 $order = &$very_bad_unclear_name; // new variable as a reference
 
+
 $order .= " and 1 can of Coke"; // extension string
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+
+/** Numbers */
+$intVariable = 8;
+
+$floatVariable = 1.5;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$difference = abs($last_month-$this_month);
+
+$message = <<<message
+Насколько больше я потратил в прошлом месяце, чем в этом месяце? 
+В прошлом месяце я потратил {$last_month}, а в этом месяце я потратил {$this_month}.
+Тогда разница между расходами в прошлом месяце и в этом месяце равна {$difference}.
+message;
+
+echo "\n$intVariable";
+echo "\n";
+echo $floatVariable;
+echo "\n$intVariable * $floatVariable"; // output = 12
+echo "\n$message";
